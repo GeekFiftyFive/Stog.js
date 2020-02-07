4e2fb8527026446348c3421c539d132a0e8b672c
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-02-07 17:06:40 +0000

    Start adding CSS to example. Continue implementation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let configPath = process.argv[2];
 if(!configPath) {
-    configPath = './stog-config.json'
+    configPath = './stog-config.json';
 }
+
+const basePathHelper = require('./src/helpers/basePathHelper');
 const config = require(`./${configPath}`);
+const basePath = basePathHelper(configPath);
