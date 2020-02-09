82d29588f7cfa3d507c356ac56a027cc78ccd178
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-02-09 21:17:02 +0000

    Use showdown to convert imported markdown to html

--- src/stog.js
+++ src/stog.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
+const showdown = require('showdown');
 
 const readdir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
 
 module.exports = async (config, basePath) => {
     let markdown = [];
+    const converter = new showdown.Converter();
 
     if(Array.isArray(config.markdown)) {
         //TODO: Implement ability to specify files by array
@@ -15,7 +17,8 @@ module.exports = async (config, basePath) => {
         filenames.forEach(async (filename) => {
             if(path.extname(filename) == '.md') {
                 let contents = await readFile(basePath + config.markdown + filename, 'utf-8');
-                console.log(contents);
+                let html = converter.makeHtml(contents);
+                console.log(html)
             }
         })
     }
