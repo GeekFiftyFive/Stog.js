b86aecc308b1cc49d767400f84040bd63d00bae7
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-02-10 17:36:06 +0000

    Create index page

--- src/stog.js
+++ src/stog.js
@@ -14,7 +14,9 @@ async function copyCSS(config, basePath) {
 }
 
 module.exports = async (config, basePath) => {
+    const outputPath = basePath + config.output;
     const converter = new showdown.Converter();
+    let markdown = [];
 
     copyCSS(config, basePath);
 
@@ -22,15 +24,42 @@ module.exports = async (config, basePath) => {
         //TODO: Implement ability to specify files by array
     } else {
         let filenames = await readdir(basePath + config.markdown);
-        const outputPath = basePath + config.output;
         filenames.forEach(async (filename) => {
             if(path.extname(filename) == '.md') {
-                let dom = await loadContent(basePath, config, filename, converter, outputPath);
+                let contents = await readFile(basePath + config.markdown + filename, 'utf-8');
+                markdown.push(contents); //Store contents for home page
+                let dom = await loadContent(contents, config, converter, outputPath);
+                writeNav(dom, config.title);
                 wrapContents(dom);
                 writeFile(outputPath + fsHelper.findFileName(filename) + '.html', dom.serialize());
             }
         })
     }
+
+    // Write the index page
+    let dom = new JSDOM();
+    writeNav(dom, config.title);
+    addStyle(dom.window.document, config);
+    writeFile(outputPath + 'index.html', dom.serialize());
+}
+
+function writeNav(dom, pageTitle) {
+    let document = dom.window.document;
+    let body = document.querySelector('body');
+    let div = document.createElement('div');
+    let title = document.createElement('h1');
+    title.textContent = pageTitle;
+    let nav = document.createElement('ul');
+    let home = document.createElement('li');
+    let homeLink = document.createElement('a');
+    homeLink.textContent = 'Home';
+    homeLink.setAttribute('href', 'index.html');
+
+    home.appendChild(homeLink);
+    nav.appendChild(home);
+    div.appendChild(title);
+    div.appendChild(nav);
+    body.appendChild(div);
 }
 
 function wrapContents(dom) {
@@ -43,8 +72,14 @@ function wrapContents(dom) {
     body.appendChild(wrapper);
 }
 
-async function loadContent(basePath, config, filename, converter, outputPath) {
-    let contents = await readFile(basePath + config.markdown + filename, 'utf-8');
+function addStyle(document, config) {
+    let style = document.createElement('link');
+    style.setAttribute('rel', 'stylesheet');
+    style.setAttribute('href', config.css);
+    document.querySelector('html').appendChild(style);
+}
+
+async function loadContent(contents, config, converter, outputPath) {
     let html = converter.makeHtml(contents);
     await fsHelper.createIfNotExists(outputPath);
     let dom = new JSDOM(html);
@@ -52,10 +87,7 @@ async function loadContent(basePath, config, filename, converter, outputPath) {
     let title = document.createElement('title');
     let primaryHeading = document.querySelector('h1').textContent;
     title.textContent = primaryHeading + ' - ' + config.title;
-    let style = document.createElement('link');
-    style.setAttribute('rel', 'stylesheet');
-    style.setAttribute('href', config.css);
-    document.querySelector('html').appendChild(style);
+    addStyle(document, config);
     document.querySelector('head').appendChild(title);
     return dom;
 }
