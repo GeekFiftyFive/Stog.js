909ac8886c5178fc578f658898f79fe72cc4099a
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-02-12 20:40:27 +0000

    Refactor to generate post objects prior to file generation

--- src/stog.js
+++ src/stog.js
@@ -16,40 +16,64 @@ async function copyCSS(config, basePath) {
 module.exports = async (config, basePath) => {
     const outputPath = basePath + config.output;
     const converter = new showdown.Converter();
-    let markdown = [];
+    let posts = [];
 
     copyCSS(config, basePath);
 
-    if(Array.isArray(config.markdown)) {
-        //TODO: Implement ability to specify files by array
-    } else {
-        let filenames = await readdir(basePath + config.markdown);
-        filenames.forEach(async (filename) => {
-            if(path.extname(filename) == '.md') {
-                let contents = await readFile(basePath + config.markdown + filename, 'utf-8');
-                markdown.push(contents); //Store contents for home page
-                let dom = await loadContent(contents, config, converter, outputPath);
-                writeNav(dom, config.title);
-                wrapContents(dom);
-                await writeFile(outputPath + fsHelper.findFileName(filename) + '.html', dom.serialize());
-                if(markdown.length == filenames.length) {
-                    await writeIndexPage(markdown, config, outputPath, converter);
-                }
+    let filenames = await readdir(basePath + config.markdown);
+    for(let i = 0; i < filenames.length; i++) {
+        const filename = filenames[i];
+        if(path.extname(filename) == '.md') {
+            let contents = await readFile(basePath + config.markdown + filename, config.encoding);
+            let html = converter.makeHtml(contents);
+            let dom = new JSDOM(html);
+
+            let post = {
+                html: html
             }
-        });
+
+            let h1s = dom.window.document.querySelectorAll('h1');
+            if(h1s.length != 1) {
+                throw Error(`Error validating ${filename}, exactly 1 h1 element must exist`);
+            }
+
+            post.title = h1s[0].textContent;
+            post.filename = filename;
+            post.tabname = post.title + ' - ' + config.title;
+            posts.push(post);
+        }
+    }
+
+    await writePosts(posts, config, outputPath);
+    await writeIndexPage(posts, config, outputPath, converter);
+}
+
+async function writePosts(posts, config, outputPath) {
+    for(let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        await fsHelper.createIfNotExists(outputPath);
+        let dom = new JSDOM(post.html);
+        let document = dom.window.document;
+        let title = document.createElement('title');
+        title.textContent = post.tabname;
+        addStyle(document, config);
+        document.querySelector('head').appendChild(title);
+        writeNav(dom, config.title);
+        wrapContents(dom);
+        await writeFile(outputPath + fsHelper.findFileName(post.filename) + '.html', dom.serialize());
     }
 }
 
-async function writeIndexPage(markdown, config, outputPath, converter) {
+async function writeIndexPage(posts, config, outputPath, converter) {
     // Write the index page
     let dom = new JSDOM();
     let document = dom.window.document;
     writeNav(dom, config.title);
 
-    for(let i = 0; i < Math.min(config.postsOnHome, markdown.length); i++) {
-        let md = markdown[i];
+    for(let i = 0; i < Math.min(config.postsOnHome, posts.length); i++) {
+        let post = posts[i];
 
-        let contentDom = new JSDOM(converter.makeHtml(md));
+        let contentDom = new JSDOM(post.html);
         let body = contentDom.window.document.querySelector('body');
         let div = document.createElement('div');
         body.childNodes.forEach(node => {
@@ -100,17 +124,4 @@ function addStyle(document, config) {
     style.setAttribute('rel', 'stylesheet');
     style.setAttribute('href', config.css);
     document.querySelector('html').appendChild(style);
-}
-
-async function loadContent(contents, config, converter, outputPath) {
-    let html = converter.makeHtml(contents);
-    await fsHelper.createIfNotExists(outputPath);
-    let dom = new JSDOM(html);
-    let document = dom.window.document;
-    let title = document.createElement('title');
-    let primaryHeading = document.querySelector('h1').textContent;
-    title.textContent = primaryHeading + ' - ' + config.title;
-    addStyle(document, config);
-    document.querySelector('head').appendChild(title);
-    return dom;
 }
